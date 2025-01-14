ba9299a70a1e1d76f6eaa0e39fec5f7b26e92fd6
Mark Smith <mark.smith@extraspace.com>
2025-01-14 10:32:20 -0700

    custom deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build",
-    sourcemap: true,
-    minify: "esbuild",
-  },
-  base: "/msmith1218/",
+  base: "/PersonalFinanceApp/",
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "^/v\\d+/.*": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      common: `${path.resolve(__dirname, "./src/common/")}`,
-      components: `${path.resolve(__dirname, "./src/components/")}`,
-    },
-  },
 });
