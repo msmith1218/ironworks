d67d3c0d7adb7d3f19b41bbad952710b49ea06a5
Mark Smith <mark.smith@extraspace.com>
2025-01-14 11:26:09 -0700

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { viteStaticCopy } from "vite-plugin-static-copy";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/PersonalFinanceApp/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./service-worker.js", // correct path to this file.
+          dest: "./", // root of your output directory
+        },
+      ],
+    }),
+  ],
 });
