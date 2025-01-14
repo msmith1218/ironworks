03dd49c918bfd3144ac1e256b5e0aa489f4f6346
Mark Smith <mark.smith@extraspace.com>
2025-01-14 11:50:15 -0700

    add bunch of vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,39 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      injectRegister: "auto",
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Vite PWA Project",
+        short_name: "Vite PWA Project",
+        theme_color: "#ffffff",
+        start_url: "index.html?fullscreen=true",
+        display: "fullscreen",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
   ],
 });
