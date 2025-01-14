7ed9c58d037623ec541ded7c87ad7db71eee593a
Mark Smith <mark.smith@extraspace.com>
2025-01-14 11:51:14 -0700

    remove old service worker reference

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,6 @@ import App from "./App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
-  });
-}
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
