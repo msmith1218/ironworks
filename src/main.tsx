aa28422794fc4651283c8745f7d06fab16843838
msmith1218 <msmith1218@gmail.com>
2025-08-14 12:01:04 -0600

    fix base path

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import App from "./App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router";
-import Home from "components/home/home";
-import Services from "components/services/services";
-import About from "components/about/about";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
