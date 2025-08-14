678913a2eb6c4375b148e575cf644c37d58ba9f7
msmith1218 <msmith1218@gmail.com>
2025-08-14 11:56:08 -0600

    fix main script

--- src/main.tsx
+++ src/main.tsx
@@ -8,16 +8,6 @@ import About from "components/about/about";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path="/" index element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="services" element={<Services />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<Home />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <App />
   </StrictMode>
 );
