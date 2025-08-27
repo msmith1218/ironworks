e9d32c63bf56d224c490c23f1e48f21a16d499c3
msmith1218 <msmith1218@gmail.com>
2025-08-27 10:55:11 -0600

    fix github build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/",
-  resolve: {
-    alias: {
-      common: `${path.resolve(__dirname, "./src/common/")}`,
-      components: `${path.resolve(__dirname, "./src/components/")}`,
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    base: env.VITE_BASE_PATH || "/",
+    resolve: {
+      alias: {
+        common: `${path.resolve(__dirname, "./src/common/")}`,
+        components: `${path.resolve(__dirname, "./src/components/")}`,
+      },
     },
-  },
-  plugins: [
-    react(),
-  ],
+    plugins: [
+      react(),
+    ],
+  };
 });
